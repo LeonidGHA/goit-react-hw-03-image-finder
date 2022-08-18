ba2496abd96da8f584e2d63cc00b2679590276a2
LeonidGHA <stowk1992@gmail.com>
2022-08-18 18:01:34 +0300

    add start file hw

---
+++ src/components/App.jsx
@@ -0,0 +1,26 @@
+import { Component } from 'react';
+
+class App extends Component {
+  state = {
+    hello: '',
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        React homework template
+      </div>
+    );
+  }
+}
+
+export default App;
